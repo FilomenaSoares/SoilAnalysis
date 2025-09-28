8b8577a50e24ff5f2c914cb454f46b4e15f97344
Filomena <filomenasoares21@gmail.com>
2025-09-28 17:34:05 -0300

    fix: urls default

--- soilanalysis/urls.py
+++ soilanalysis/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from DHT11.views import dht11_frontend, DHT11ViewSet
+from django.shortcuts import redirect
+from DHT11.views import DHT11ViewSet
 
+# Configurações do DRF
 router = DefaultRouter()
 router.register(r'dados', DHT11ViewSet, basename='dht11')
 
 urlpatterns = [
-    path('api/', include(router.urls)),    #api para jason
-    path('dht11/', include('DHT11.urls')),  #frontend - 8000/dht11/frontend/
+    path('api/', include(router.urls)),          # API JSON
+    path('dht11/', include('DHT11.urls')),      # Frontend do DHT11
+    path('', lambda request: redirect('dht11/frontend/')),  # Raiz redireciona para frontend
 ]
-from django.urls import path, include
-
-
