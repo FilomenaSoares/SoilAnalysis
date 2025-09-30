564d095bae275654db75eb8df6d9c99019e3fdbf
Filomena da Silva Soares <filomena.soares@cuiaba.mt.gov.br>
2025-09-30 10:00:10 -0400

    fix: fiz mqtt

--- soilanalysis/python-paho-hivemq-cloud/mqtt_client.py
+++ soilanalysis/python-paho-hivemq-cloud/mqtt_client.py
@@ -1,19 +1,7 @@
-#
-# Copyright 2021 HiveMQ GmbH
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+# mqtt_client.py
+
 import time
+import ssl
 import paho.mqtt.client as paho
 from paho import mqtt
 import sys
@@ -21,68 +9,73 @@ import os
 import json
 from datetime import datetime
 
+# Configurando o Django
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "soilanalysis.settings")
 
 import django
 django.setup()
 
-from DHT11.models import DHT11Data
+from DHT11.models import DHT11Data  # Importa seu modelo Django
 
-# setting callbacks for different events to see if it works, print the message etc.
+# ========== CONFIGURAÇÕES DO MQTT ==========
+MQTT_BROKER = "SUA_URL_DO_BROKER"         # Ex: "abc123.s2.eu.hivemq.cloud"
+MQTT_PORT = 8883                          # Porta segura padrão para MQTT com TLS
+MQTT_USERNAME = "seu_usuario"
+MQTT_PASSWORD = "sua_senha"
+MQTT_TOPIC = "sensores/dht11"            # Altere para o tópico usado pelo sensor
+# ============================================
+
+# Callback de conexão
 def on_connect(client, userdata, flags, rc, properties=None):
-    print("CONNACK received with code %s." % rc)
+    if rc == 0:
+        print(" Conectado com sucesso ao broker MQTT")
+        client.subscribe(MQTT_TOPIC, qos=1)
+    else:
+        print(f" Falha na conexão. Código: {rc}")
 
-# with this callback you can see if your publish was successful
-def on_publish(client, userdata, mid, properties=None):
-    print("mid: " + str(mid))
-
-# print which topic was subscribed to
-def on_subscribe(client, userdata, mid, granted_qos, properties=None):
-    print("Subscribed: " + str(mid) + " " + str(granted_qos))
-
-# print message, useful for checking if it was successful
+# Callback de mensagem recebida
 def on_message(client, userdata, msg):
     try:
         data = json.loads(msg.payload.decode())
         temp = data.get("temperature")
         hum = data.get("humidity")
 
-        DHT11Data.objects.create(
-            temperatura=temp,
-            umidade=hum,
-            ttimestamp=datetime.now()
-        )
-
-        print(f"Dados salvos: Temp={temp}°C, Hum={hum}%, Timestamp={time.strftime('%d-%m-%Y %H:%M:%S')}")
+        if temp is not None and hum is not None:
+            DHT11Data.objects.create(
+                temperatura=temp,
+                umidade=hum,
+                timestamp=datetime.now()
+            )
+            print(f" Dados salvos: Temp={temp}°C, Hum={hum}%, Timestamp={datetime.now().strftime('%d-%m-%Y %H:%M:%S')}")
+        else:
+            print(" Dados incompletos ou inválidos recebidos.")
     except Exception as e:
         print(f" Erro ao processar mensagem: {e}")
 
-# using MQTT version 5 here, for 3.1.1: MQTTv311, 3.1: MQTTv31
-# userdata is user defined data of any type, updated by user_data_set()
-# client_id is the given name of the client
+# Callback de inscrição
+def on_subscribe(client, userdata, mid, granted_qos, properties=None):
+    print(f" Inscrito no tópico: {MQTT_TOPIC} com QoS {granted_qos[0]}")
+
+# Callback de publicação (caso publique algo)
+def on_publish(client, userdata, mid, properties=None):
+    print(f"Mensagem publicada. mid: {mid}")
+
+# Criação do cliente MQTT
 client = paho.Client(client_id="", userdata=None, protocol=paho.MQTTv5)
 client.on_connect = on_connect
-
-# enable TLS for secure connection
-client.tls_set(tls_version=mqtt.client.ssl.PROTOCOL_TLS)
-# set username and password
-client.username_pw_set("usuario", "senha")
-# connect to HiveMQ Cloud on port 8883 (default for MQTT)
-client.connect("#### url", 8883)
-
-# setting callbacks, use separate functions like above for better visibility
-client.on_subscribe = on_subscribe
 client.on_message = on_message
+client.on_subscribe = on_subscribe
 client.on_publish = on_publish
 
-# subscribe to all topics of encyclopedia by using the wildcard "#"
-client.subscribe("encyclopedia/#", qos=1)
+# Configura TLS (conexão segura com HiveMQ Cloud)
+client.tls_set(tls_version=ssl.PROTOCOL_TLS)
 
-# a single publish, this can also be done in loops, etc.
-client.publish("encyclopedia/temperature", payload="hot", qos=1)
+# Autenticação
+client.username_pw_set(MQTT_USERNAME, MQTT_PASSWORD)
 
-# loop_forever for simplicity, here you need to stop the loop manually
-# you can also use loop_start and loop_stop
+# Conecta ao broker
+client.connect(MQTT_BROKER, MQTT_PORT)
+
+# Inicia o loop
 client.loop_forever()
